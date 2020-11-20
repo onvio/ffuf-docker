1b2210aa03dd1832da2d96003601a4c5ee8cc288
root <d.snel@onvio.nl>
2020-11-20 17:03:10 +0100

    Add random useragent

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -x
 
 # todo if FUZZ is missing in URL add it by default
-ffuf -w /opt/ffuf/words_and_files_small.txt -ac -u $1/FUZZ -H "X-Scanner: FFUF" -of csv -o /var/reports/ffuf_scan.csv -r -mc 200,301,401,403,405 ${@:2}
+ffuf -w /opt/ffuf/words_and_files_small.txt -ac -u $1/FUZZ -H "X-Scanner: FFUF" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" -of csv -o /var/reports/ffuf_scan.csv -r -mc 200,301,401,403,405 ${@:2}
 
 # remove header
 sed -i 1d /var/reports/ffuf_scan.csv
