a6b83c24b7be6811c7e87a3c256afc4838e90b5b
bryan_onvio <b.soman@onvio.nl>
2020-11-20 13:41:11 +0100

    optional ffuf arguments

--- ffuf.sh
+++ ffuf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-f=$1
+args="$@"
 
-ffuf -w words_and_files_small.txt -u $1/FUZZ -H "X-Scanner: ZAP" -of csv -o /var/reports/ffuf_scan.csv -r -mc 200,301,401,403,405
+ffuf -w words_and_files_small.txt -u $1/FUZZ -H "X-Scanner: ZAP" -of csv -o /var/reports/ffuf_scan.csv -r -mc 200,301,401,403,405 $2
--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
+args="$@"
 
 ## Remove protocol part of url for IO operations##
 f=$1
 f="${f#http://}"
 f="${f#https://}"
 f=${f%%/*}
-echo "$f"
 
-bash /opt/ffuf/ffuf.sh $1
+bash /opt/ffuf/ffuf.sh $1 "$2"
 /opt/ffuf/sort_ffuf.sh
 sleep 2
 python3 /opt/ffuf/ffuf_qt.py
