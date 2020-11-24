1aaa7ac6840c8f9666b6cdc9d2e3c6fe3ae3ca18
root <d.snel@onvio.nl>
2020-11-24 10:54:04 +0100

    Don't crash when Ffuf fails

--- ffuf_qt.py
+++ ffuf_qt.py
@@ -1,6 +1,6 @@
 import csv
 import json
-import sys
+import logging
 
 data = {
     "vulnerabilities": []
@@ -8,16 +8,19 @@ data = {
 
 report_path = "/var/reports/ffuf.json"
 
-with open('/var/reports/ffuf_scan.csv', 'r') as csvfile:
-    report_reader = csv.reader(csvfile)
-    # Skip the header
-    next(report_reader)
-    for row in report_reader:
-        data['vulnerabilities'].append({
-            'title': 'Directory / File Detected',
-            'description': row[1], # Full URL with fuzzed path
-            'severity': "medium"
-        })
+try:
+    with open('/var/reports/ffuf_scan.csv', 'r') as csvfile:
+        report_reader = csv.reader(csvfile)
+        # Skip the header
+        next(report_reader)
+        for row in report_reader:
+            data['vulnerabilities'].append({
+                'title': 'Directory / File Detected',
+                'description': row[1], # Full URL with fuzzed path
+                'severity': "medium"
+            })
 
-with open(report_path, 'w') as outfile:
-    json.dump(data, outfile)
+    with open(report_path, 'w') as outfile:
+        json.dump(data, outfile)
+except FileNotFoundError:
+    logging.error('Ffuf scan report not found')
